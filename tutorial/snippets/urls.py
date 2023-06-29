d9ee31296b7734744796573e5f7c7928bc5797f0
Гаяне <gayanev.stepanyan@yandex.ru>
2023-06-29 07:36:49 +0300

    Object level permissions

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 from django.urls import path, include
 
+
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
